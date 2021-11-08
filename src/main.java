55e709a0c20f0c5519421a404b4db535d75acf7d
axxxkh <alxxxkh@gmail.com>
2021-11-08 20:19:34 +0200

    ddfdhh

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("dddd");
         System.out.println("whyyy");
+        System.out.println("ffff");
     }
 }
